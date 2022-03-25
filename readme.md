6cbb5657ca1dc213c23a463f3741f1d4392c6adf
FranSantander <naturatiendacopiapo@gmail.com>
2022-03-25 20:06:09 -0300

    comentado el script y documentado en readme

---
+++ readme.md
@@ -0,0 +1,9 @@
+Requerimientos:   
+1.Hacer uso de Async/Await para las funciones que consulten los endpoints de la pokeapi.  
+2.Usar el Promise.all() para ejecutar yo btener la data de las funciones asíncronas generando un nuevo arreglo con la data a entregar en el siguiente requerimiento.   
+3.Disponibilizar la ruta http://localhost:3000/pokemones  que devuelva un JSON con el nombre y la url de una imagen de 150 pokemones, asi como verás en la siguiente imagen. 
+
+Para funcionar el servidor, en la terminal escribir:
+1.- npm install nodemon
+2.- nodemon server,js
+3.- Ingresar a localhost:3000/pokemones
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
+//Constante para importar las dependencias del script a utilizar
 const axios = require("axios");
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+//Función vacía para luego llenar los 151 pokemones
 let todosPokemones = [];
 let pokemonesPromesas = [];
 
+//Creando el servidor
 http
   .createServer((req, res) => {
-    if (req.url.startsWith("/pagina")){
+    if (req.url.startsWith("/pagina")){//lectura del html
         res.writeHead(200, { "Content-Type": "text/html"})
         fs.readFile("index.html", "utf8", (err, html) => {
             res.end(html);
           });
 
     }
-
-    if (req.url.startsWith("/pokemones")) {
+    //Requerimiento 3
+    if (req.url.startsWith("/pokemones")) {//muestra el arreglo
       res.writeHead(200, { "Content-Type": "application/json" });
       async function pokemonesGet() {
         const { data } = await axios.get(
@@ -24,19 +27,21 @@ http
         );
         return data.results;
       }
-
+    //Requerimiento 1
       async function getFullData(url) {
         const { data } = await axios.get(url);
         return data;
       }
-
+      //Recorrido la url para los datos de los 151 pokemones
       pokemonesGet().then((results) => {
         results.forEach((p) => {
           let pokemonUrl = p.url;
-          pokemonesPromesas.push(getFullData(pokemonUrl));
+          pokemonesPromesas.push(getFullData(pokemonUrl));//acceso a la info 1 pokemon
         });
+        //Requerimiento 2: arreglo de promesas
         Promise.all(pokemonesPromesas).then((data) => {
           data.forEach((p) => {
+            //formato de objeto para "llenar" el arreglo vacío todosPokemones
             let pokemon = {
               nombre: `${p.name}`,
               img: `${p.sprites["front_default"]}`,
@@ -44,7 +49,7 @@ http
             todosPokemones.push(pokemon);
             //console.log(`${p.name}`);
           });
-          res.write(JSON.stringify(todosPokemones))
+          res.write(JSON.stringify(todosPokemones))//transforma a formato JSON
           res.end()
         });
       });
